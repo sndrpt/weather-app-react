cb59bfac374d1d33403f0b7b2f780eb899f38475
sndrpt <103604366+sndrpt@users.noreply.github.com>
2022-12-04 13:12:09 +0100

    Added axios integration - showing current weather

---
+++ src/Weather.js
@@ -0,0 +1,14 @@
+import React from "react";
+import axios from "axios";
+
+export default function Weather(props) {
+  function handleResponse(response) {
+    alert(
+      `It is ${Math.round(response.data.main.temp)}°C in ${response.data.name}.`
+    );
+  }
+
+  let apiKey = "c0579156688656753a8395372ffe7755";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${props.city}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(handleResponse);
+}
