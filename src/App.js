b26a08056e895ba80e231fdc0401b057bf9d61de
sndrpt <103604366+sndrpt@users.noreply.github.com>
2022-12-04 13:21:40 +0100

    updated content

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <h1>Hello</h1>
-
+        <h4>Have a look at the weather in your city.</h4>
         <Weather city="Paris" />
       </header>
     </div>
